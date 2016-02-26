e1bb76e83fff450020b1eb4af024de29ea9e4ead
Gary Thomas <garlos@gmail.com>
2016-02-25 20:26:34 -0800

    Add additional properties to store, add styling

--- components/Clock.js
+++ components/Clock.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './Clock.css'
 
 class Clock extends Component {
   constructor (props) {
@@ -7,9 +8,20 @@ class Clock extends Component {
 
   render () {
     const { time } = this.props
-
     return (
-      <div>{ time.toTimeString() }</div>
+<pre>
+<span className="on">IT</span>L<span className="on">IS</span>BFAMPM<br/>
+<span className={time.quarter ? 'on' : ''}>A</span>C<span className={time.quarter ? 'on' : ''}>QUARTER</span>DC<br/>
+<span className={time.twentyMinutes ? 'on' : ''}>TWENTY</span><span className={time.fiveMinutes ? 'on' : ''}>FIVE</span>X<br/>
+<span className={time.half ? 'on' : ''}>HALF</span>B<span className={time.tenMinutes ? 'on' : ''}>TEN</span>F<span className={time.to ? 'on' : ''}>TO</span><br/>
+<span className={time.past ? 'on' : ''}>PAST</span>ERU<span className={time.h == 9 ? 'on' : ''}>NINE</span><br/>
+<span className={time.h == 1 ? 'on' : ''}>ONE</span><span className={time.h == 6 ? 'on' : ''}>SIX</span><span className={time.h == 3 ? 'on' : ''}>THREE</span><br/>
+<span className={time.h == 4 ? 'on' : ''}>FOUR</span><span className={time.h == 5 ? 'on' : ''}>FIVE</span><span className={time.h == 2 ? 'on' : ''}>TWO</span><br/>
+<span className={time.h == 8 ? 'on' : ''}>EIGHT</span><span className={time.h == 11 ? 'on' : ''}>ELEVEN</span><br/>
+<span className={time.h == 7 ? 'on' : ''}>SEVEN</span><span className={time.h == 0 ? 'on' : ''}>TWELVE</span><br/>
+<span className={time.h == 10 ? 'on' : ''}>TEN</span>SE<span className={time.oclock ? 'on' : ''}>OCLOCK</span><br/>
+<span className={time.oneMinute ? 'on' : ''}>.</span>  <span className={time.twoMinutes ? 'on' : ''}>.</span>  <span className={time.threeMinutes ? 'on' : ''}>.</span>  <span className={time.fourMinutes ? 'on' : ''}>.</span>
+</pre>
     )
   }
 }
--- reducers/index.js
+++ reducers/index.js
@@ -5,7 +5,24 @@ export default function counter (state = initialState, action) {
     case 'TICK':
       const { now } = action;
 
-      return now
+      const m = now.getMinutes()
+
+      return {
+        m,
+        h: (now.getHours() + (m >= 40 ? 1 : 0)) % 12,
+        past: m >= 5 && m < 35,
+        to: m >= 35,
+        oclock: m >= 0 && m < 5,
+        oneMinute: m % 5 >= 1,
+        twoMinutes: m % 5 >= 2,
+        threeMinutes: m % 5 >= 3,
+        fourMinutes: m % 5 >= 4,
+        fiveMinutes: (m >= 5 && m < 10) || (m >= 55) || (m >= 25 && m < 30) || (m >= 35 && m < 40),
+        tenMinutes: (m >= 10 && m < 15) || (m >= 50 && m < 55),
+        quarter: (m >= 15 && m < 20) || (m >= 45 && m < 50),
+        twentyMinutes: (m >= 20 && m < 30) || (m >= 35 && m < 45),
+        half: (m >= 30 && m < 35)
+      }
     default:
       return state
   }
